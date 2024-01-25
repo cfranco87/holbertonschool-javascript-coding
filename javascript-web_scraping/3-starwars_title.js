571040621a1dbf7dc680b1287509a9f994d4ee16
root <carlosfrancoart@gmail.com>
2024-01-25 18:56:52 -0300

    star wars

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const fs = require('request');
+const urlApi = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(urlApi, function( error, response, body) {
+    if (error) {
+        console.log(error);
+      } else {
+        console.log(JSON.parse(body).title); 
+      }
+});
