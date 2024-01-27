94b3842fe21fb4493aee422d8c08fffe7eb757a8
root <carlosfrancoart@gmail.com>
2024-01-27 20:08:52 -0300

    filename var

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const fileName = process.argv[2];
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(fileName, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
